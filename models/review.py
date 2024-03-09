1fd2ddfdf630085336e3756130ab718c39cbc371
AntipasG <antipasbelihu@gmail.com>
2024-03-09 21:16:37 +0300

    review

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class Review is being created"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Class review is created that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    test = ""
