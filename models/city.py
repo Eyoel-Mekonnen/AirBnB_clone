5961eda0747976c98b948caf0bfc6735d0ecfa43
AntipasG <antipasbelihu@gmail.com>
2024-03-09 21:15:22 +0300

    city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class City is being created"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Inherits BaseModel class"""
+    state_id = ""
+    name = ""
