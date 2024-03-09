95630b6f1e6b8ac733ebf35aed94d7570aca9f7d
AntipasG <antipasbelihu@gmail.com>
2024-03-09 21:07:02 +0300

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class Amenity is created"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity is creatded which inherits from Basemodel"""
+    name = ""
