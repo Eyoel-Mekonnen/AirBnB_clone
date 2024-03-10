b9766a7e6cef6f84ed5902c9311839d361267691
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-10 11:02:25 +0300

    task

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
                 self.created_at = datetime.now()
             if 'updated_at' not in kwargs:
                 self.updated_at = datetime.now()
-        elif len(kwargs) == 0:
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
