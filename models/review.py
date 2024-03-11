919750c72dc6e7104ba4e896346ae2c1eb64c788
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-11 14:35:32 +0300

    hot_fix

--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 """Class Review is being created"""
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """Class review is created that inherits from BaseModel"""
     place_id = ""
--- models/user.py
+++ models/user.py
@@ -2,10 +2,10 @@
 """class User is being created"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """Class user created that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
